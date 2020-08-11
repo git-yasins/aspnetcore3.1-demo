6ac17ec80ad5daf061d25c5ee8aa730713110d33
git-yasins <yasins@foxmail.com>
2020-08-11 15:08:35 +0800

    Redis init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspnetcore3_demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
