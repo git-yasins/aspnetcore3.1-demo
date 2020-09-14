42180e34b08d833eb33c379cdec7f33206becc2b
git-yasins <2389092255@qq.com>
2020-09-14 15:29:47 +0800

    init

--- Program.cs
+++ Program.cs
@@ -1,34 +1,30 @@
 using System;
-using aspnetcore3_demo.Data;
-using Autofac.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.Collections.Immutable;
+using Microsoft.Extensions.Configuration;
 
-namespace aspnetcore3._1_demo {
+namespace aspnetcore3_demo {
     public class Program {
         public static void Main (string[] args) {
-            var host = CreateHostBuilder (args).Build ();
-            using (var scope = host.Services.CreateScope ()) {
-                try {
-                    var dbContext = scope.ServiceProvider.GetService<RoutineDBContext> ();
-                    dbContext.Database.EnsureDeleted ();
-                    dbContext.Database.Migrate ();
-                } catch (Exception ex) {
-                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>> ();
-                    logger.LogError (ex, "Database Migration Error!");
-                }
-            }
-            host.Run ();
-        }
+            var builder = new ConfigurationBuilder ();
+            builder.AddEnvironmentVariables ();
 
-        public static IHostBuilder CreateHostBuilder (string[] args) =>
-            Host.CreateDefaultBuilder (args)
-            .UseServiceProviderFactory (new AutofacServiceProviderFactory ())//使用AUTOFAC
-            .ConfigureWebHostDefaults (webBuilder => {
-                webBuilder.UseStartup<Startup> ();
-            });
+            var configurationRoot = builder.Build ();
+            System.Console.WriteLine ($"key1:{configurationRoot["key1"]}");
+
+            #region 分层键获取
+            var section = configurationRoot.GetSection ("SECTION1");
+            System.Console.WriteLine ($"KEY3:{section["KEY3"]}");
+
+            var section2 = configurationRoot.GetSection ("SECTION1:SECTION2");
+            System.Console.WriteLine ($"KEY4:{section2["KEY4"]}");
+            #endregion
+
+            #region 前缀过滤
+            builder.AddEnvironmentVariables ("XIAO_");
+            configurationRoot = builder.Build ();
+            System.Console.WriteLine ($"key1:{configurationRoot["key1"]}");
+            #endregion
+            Console.ReadKey ();
+        }
     }
 }
