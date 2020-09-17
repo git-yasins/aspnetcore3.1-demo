6aaba04d8beed038b9fc1dbc2a9714fa83b303ed
git-yasins <2389092255@qq.com>
2020-09-18 00:15:50 +0800

    temp

--- Program.cs
+++ Program.cs
@@ -5,26 +5,9 @@ using Microsoft.Extensions.Configuration;
 namespace aspnetcore3_demo {
     public class Program {
         public static void Main (string[] args) {
-            var builder = new ConfigurationBuilder ();
-            builder.AddEnvironmentVariables ();
-
-            var configurationRoot = builder.Build ();
-            System.Console.WriteLine ($"key1:{configurationRoot["key1"]}");
-
-            #region 分层键获取
-            var section = configurationRoot.GetSection ("SECTION1");
-            System.Console.WriteLine ($"KEY3:{section["KEY3"]}");
-
-            var section2 = configurationRoot.GetSection ("SECTION1:SECTION2");
-            System.Console.WriteLine ($"KEY4:{section2["KEY4"]}");
-            #endregion
-
-            #region 前缀过滤
-            builder.AddEnvironmentVariables ("XIAO_");
-            configurationRoot = builder.Build ();
-            System.Console.WriteLine ($"key1:{configurationRoot["key1"]}");
-            #endregion
-            Console.ReadKey ();
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json");
+            
         }
     }
 }
