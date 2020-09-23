08e9302ac2620062e8fbb226f6f1c26d252e3c9d
git-yasins <2389092255@qq.com>
2020-09-23 12:31:33 +0800

    configuration

--- Configuration/Program.cs
+++ Configuration/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using Microsoft.Extensions.Configuration;
 
-namespace Configuration
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-           
+namespace Configuration {
+    class Program {
+        static void Main (string[] args) {
+            IConfiguration configuration = new ConfigurationBuilder ().SetBasePath (Environment.CurrentDirectory).AddJsonFile ("appsettings.json").Build ();
+            System.Console.WriteLine(configuration["name"]);
+
         }
     }
 }
