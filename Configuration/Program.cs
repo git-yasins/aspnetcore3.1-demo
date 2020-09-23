6b329d36ca84e175397af69e53a8d0a70e854240
git-yasins <2389092255@qq.com>
2020-09-23 12:18:38 +0800

    temp

---
+++ Configuration/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Configuration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
